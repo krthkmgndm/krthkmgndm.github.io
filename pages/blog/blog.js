b587048b0ca6aea70d905db84d6cc80686f84d74
Keerthik Muruganandam <70237309+krthkmgndm@users.noreply.github.com>
2024-09-02 08:02:06 -0500

    Some more work on the blog

--- pages/blog/blog.js
+++ pages/blog/blog.js
@@ -8,10 +8,33 @@ fetch("posts.json")
     posts.forEach((post) => {
       const listItem = document.createElement("li");
       const link = document.createElement("a");
+      const span = document.createElement("span");
       link.href = post.file;
-      link.textContent = `${post.title} - ${post.date}`;
+      link.textContent = `${post.title}`;
+      span.textContent = `${post.date}`;
       listItem.appendChild(link);
       postList.appendChild(listItem);
     });
   })
   .catch((error) => console.error("Error loading posts:", error));
+
+function dateTo(str_date) {
+  arr_date = str_date.split("-");
+  months = [
+    "",
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+  ];
+
+  return months[arr_date[1]]+" "+;
+}
